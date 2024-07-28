ad232fd66752c050ad54c53383482464260e6d97
Sang_Hyeok <61928927+tkdgur7234@users.noreply.github.com>
2024-07-28 20:22:05 +0900

    Title: 특정 숫자 도달하기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240728/특정 숫자 도달하기/reaching-specific-number.py
+++ 240728/특정 숫자 도달하기/reaching-specific-number.py
@@ -1,7 +1,9 @@
 arr = list(map(int, input().split()))
 plus = 0
+num = 0;
 for i in range(0, len(arr)):
     if arr[i] >= 250:
         break
     plus += arr[i]
-print(f"{plus} {plus/i:.1f}")
+    num += 1
+print(f"{plus} {plus/num:.1f}")
